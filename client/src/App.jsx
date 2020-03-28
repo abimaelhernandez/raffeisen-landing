239cb14e4e0be6560dd494e3cb8d90b1041ec787
Javier Elizalde <javier.elizalde@globant.com>
2020-03-28 14:04:06 -0600

    Added bootstrap to project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,6 @@ export default class App extends Component {
       <div className="parent-container">
         <h1>Hello World we are here are we</h1>
         <h2> hi there here we are are tou </h2>
-          <span> hey im </span>
       </div>
     )
   }
