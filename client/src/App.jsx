753db94764e97de658fbb2a459d257a808554a5c
abimael-hernandez <abimael.hernandez@adevinta.com>
2020-03-27 23:21:07 -0600

    have installed sass in the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import '../dist/styles.scss';
+
 export default class App extends Component {
   render() {
-    return <h1>Hello World</h1>
+    return (
+      <div className="parent-container">
+        <h1>Hello World we are here are we</h1>
+        <h2> hi there here we are</h2>
+      </div>
+    );
   }
 }
