67f81714d7eeced33b7199c497a89f353b8c4a82
Javier Elizalde <javier.elizalde@globant.com>
2020-04-10 16:42:14 -0500

    Created footer and resolved issue with url loader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import NavBar from './components/navigation'
 import Carousela from './components/Carousel'
+import FooteR from './components/Footer'
 
 import '../dist/styles.scss'
 
@@ -10,6 +11,7 @@ export default class App extends Component {
       <div className="parent-container">
         <NavBar/>
         <Carousela/>
+        <FooteR/>
       </div>
     )
   }
