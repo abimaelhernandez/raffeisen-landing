15556794490fab3ab6c1397a7f92561018923466
abimael-hernandez <abimael.hernandez@adevinta.com>
2020-04-12 20:11:48 -0500

    props not passing correctly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,7 @@
-// import React, { Component } from 'react'
-// import { IntlProvider, FormattedMessage } from 'react-intl'
-// import NavBar from './components/Navigation'
-// // import Carousela from './components/Carousel'
-// // import Inter from './components/Internalization'
-// // import PageFooter from './components/Footer'
-//
-// import './components/sass/mains.scss'
-//
-// export default class App extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       locale: '',
-//       setLocale: ''
-//     }
-//   }
-//
-//   handleSelect = (e) => {
-//     this.setLocale(e.target.value)
-//   }
-//
-//   render() {
-//     return (
-//       <div className="parent-container">
-//         <select onChange={this.state.handleSelect} defaultValue={this.state.locale}>
-//           {['en', 'es'].map(l => (
-//             <option key={l}>{l}</option>
-//           ))}
-//         </select>
-//         <IntlProvider locale={locale} messages={this.projectBody.messages[this.locale]}>
-//         <NavBar test={something}/>
-//           <p>
-//             <FormattedMessage id='greeting' values={{ name }} />
-//           </p>
-//           <NavBar/>
-//         </IntlProvider>
-//       </div>
-//     )
-//   }
-// }
-
 import { IntlProvider, FormattedMessage } from 'react-intl'
 import React, { Component } from 'react'
 import Navbar from './components/Navigation'
+import './components/sass/mains.scss'
 
 export default class App extends Component {
   constructor(){
@@ -50,7 +9,7 @@ export default class App extends Component {
     this.state = {
       locale: 'en',
       name: '',
-      messages : {
+      messages: {
         en: {
           greeting: `Hello ! How are you?`,
           time: 'The time is {t, time, short}.',
@@ -67,45 +26,33 @@ export default class App extends Component {
     }
   }
 
-  handleChange = (event) => {
-    console.log('in Change :', event)
-    // this.setState({ name: e.target.value})
+  handleSpanish = (e) => {
+    e.preventDefault()
+    this.setState({ locale : 'es'})
+    console.log('in Change :', this.state)
   }
 
-  handleSelect = (e) => {
-    console.log('in SELECT', e.target)
-    // this.setState({ locale: e.target.value })
-    // setName(e.target.value)
-  }
-
-  keyPress = (e) => {
-     if(e.keyCode === 'enter'){
-        console.log('value', e.target.value)
-        // put the login here
-     }
+  handleEnglish = (e) => {
+    e.preventDefault()
+    this.setState({ locale : 'en' })
+    console.log('in Change EN :', this.state)
   }
 
   render(){
     const { locale, name, messages } = this.state
     return (
       <div>
-        <button type="button" name="es" onClick={this.handleChange.bind(this)}>click</button>
-        <select onChange={this.handleSelect}
-          defaultValue={locale}>
-          {['en', 'es'].map(l => (
-          <option key={l}>{l}</option>
-        ))}
-        </select>
-
+        <button type="button" name="es" onClick={this.handleSpanish}>ES</button>
+        <button type="button" name="es" onClick={this.handleEnglish}>EN</button>
         <IntlProvider locale={locale} messages={messages[locale]}>
         <p>
           <FormattedMessage id="greeting" values={ name } />
         <br />
           <FormattedMessage id="time" values={{ t: Date.now() }} />
         <br />
-          <FormattedMessage id="date" values={{ d: Date.now() }} />
+          <FormattedMessage id="example" values={{ d: Date.now() }} />
         </p>
-          <Navbar/>
+          <Navbar tesProp={messages[locale].example}/>
         </IntlProvider>
       </div>
     )
--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,40 +1,21 @@
 
 
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
 
 export default class NavBar extends Component {
+
   render() {
+    const { example } = this.props
     return (
       <div className="navigation">
-        <div className="navigation__mainLogo">
-          <img src="../assets/logo-main.png" alt="Main Logo" className="navigation__mainLogo-img"/>
-        </div>
-        <div className="navigation__items">
-          <span className="navigation__items-item">Inicio</span>
-          <span className="navigation__items-item">Nosotros</span>
-          <span className="navigation__items-item">Servicios</span>
-          <span className="navigation__items-item">Herramientas</span>
-          <span className="navigation__items-item">Cobertura</span>
-        </div>
-        <div className="navigation__search">
-          <form action="#">
-            <input type="text" name="search" className="navigation__search-box"/>
-            <span type="submit" className="navigation__search-button">
-              <svg width="1em" height="1em">
-                <path fillRule="evenodd" d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z" clipRule="evenodd"/>
-                <path fillRule="evenodd" d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z" clipRule="evenodd"/>
-              </svg>
-            </span>
-          </form>
-        </div>
-        <div className="navigation__icons">
-          <span className="navigation__item">Contacto</span>
-          <img src="https://img.icons8.com/small/30/000000/share.png" alt="share" className="navigation__icons-individual__share"/>
-          <span className="navigation__icons-individual">Es</span>
-          <span className="navigation__icons-individual">En</span>
-          <img src="https://img.icons8.com/offices/30/000000/twitter.png" alt="Twitter" className="navigation__icons-individual__twitter"/>
-        </div>
+        {example} Hello World
       </div>
     )
   }
+}
+
+NavBar.propTypes = {
+  example: PropTypes.string
 }
