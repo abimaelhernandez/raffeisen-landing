2c83e35841e1b7000943d0ecfb558e0b800ecccf
Javier Elizalde <javier.elizalde@globant.com>
2020-04-25 18:20:27 -0500

    Modified classnames, added phone onclick functionality

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,31 +5,34 @@ export default class PageFooter extends Component {
   render() {
     return (
       <footer className="footer">
-        <div className="footer__items">
-          <img src="../assets/Twitter-white.svg" alt="twitter-logo" className="footer__icons"/>
+        <div className="footer-items">
+          <img src="../assets/Twitter-white.svg" alt="twitter-logo" className="footer-icons"/>
           <span><a href="https://twitter.com/RaiffeisenLatam" target="_blank" rel="noopener noreferrer">@RaiffeisenLatam</a></span>
         </div>
-        <div className="footer__items">
-          <img src="../assets/Email.svg" alt="email-logo" className="footer__icons"/>
+        <div className="footer-items">
+          <img src="../assets/Email.svg" alt="email-logo" className="footer-icons"/>
           <span><strong><a href="mailto:info@rflatina.com" target="_blank" rel="noopener noreferrer">info@rflatina.com</a></strong></span>
         </div>       
-        <div className="footer__items">
-            <img src="../assets/Phone.svg" alt="phone-logo" className="footer__icons"/>
-            <span>Ciudad de México, México +52.55.5037.5992 | +52.55.6273.8897<span>
+        <div className="footer-items">
+            <img src="../assets/Phone.svg" alt="phone-logo" className="footer-icons"/>
+            <span>Ciudad de México, México 
+              <a href="tel:+52-55-5037-5992"> +52.55.5037.5992</a> | 
+              <a href="tel:+52-55-6273-8897">   +52.55.6273.8897</a>
+            <span>
               <br/>
-            </span>Asunción, Paraguay +595.21.62.996</span>
+            </span>Asunción, Paraguay <a href="tel:+595-21-62-996">+595.21.62.996</a></span>
         </div>
-        <div className="footer__items">
-          <img src="../assets/Globe.svg" alt="globe-logo" className="footer__icons"/>
+        <div className="footer-items">
+          <img src="../assets/Globe.svg" alt="globe-logo" className="footer-icons"/>
           <span>Confederación Alemana de Cooperativas <strong><a href="https://www.dgrv.org" target="_blank" rel="noopener noreferrer">www.dgrv.org</a></strong></span>
         </div>
         <br/>
-        <div className="footer__dots">
-          <span className="footer__dots__dot"></span>
-          <span className="footer__dots__dot"></span>
-          <span className="footer__dots__dot"></span>
-          <span className="footer__dots__dot"></span>
-          <span className="footer__dots__dot"></span>
+        <div className="footer-dots">
+          <span className="footer-dots-dot"></span>
+          <span className="footer-dots-dot"></span>
+          <span className="footer-dots-dot"></span>
+          <span className="footer-dots-dot"></span>
+          <span className="footer-dots-dot"></span>
         </div>
       </footer>
     )
