7b03bca668cd396bc4ac1ba59926c3baf98847b4
Javier Elizalde <javier.elizalde@globant.com>
2020-04-29 21:18:59 -0500

    Made the component fully responsive for desktop and mobile

--- client/src/components/LandingBanner.jsx
+++ client/src/components/LandingBanner.jsx
@@ -7,8 +7,13 @@ export default class LandingBanner extends Component {
   render() {
     return (
       <div className="landingBanner" id='hideMe'>
-        <div className="landingBanner-container">
-         <img src="../assets/Raiffeisen-White.svg" alt="Main Logo" className="landingBanner-container-mainLogo"/>
+        <div className="landingBanner-mainContainer">
+         <img src="../assets/Raiffeisen-White.svg" alt="Main Logo" className="landingBanner-mainContainer-mainLogo"/>
+        </div>
+        <div className="landingBanner-secondaryContainer">
+          <img src="../assets/Analitica.svg" alt="Analitica Logo" className="landingBanner-secondaryContainer-analitica"/>
+          <img src="../assets/Consultoria.svg" alt="Consultoria Logo" className="landingBanner-secondaryContainer-consultoria"/>
+          <img src="../assets/Capacitacion.svg" alt="Capacitacion Logo" className="landingBanner-secondaryContainer-capacitacion"/>
         </div>
       </div>
     )
