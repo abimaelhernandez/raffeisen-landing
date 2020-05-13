5efef6e02333be799b70fd6d9d38a40a94f0586e
Javier Elizalde <javier.elizalde@globant.com>
2020-05-12 21:42:22 -0500

    slider shows, you can activate and deactivate it. No styling at all

--- client/src/components/ToolsSlider.jsx
+++ client/src/components/ToolsSlider.jsx
@@ -9,15 +9,43 @@ export default class ToolsSlider extends Component {
     resetProps(true)
   }
 
-  render(){
+  getActive = (item) => {
     const { clickedId } = this.props
+    console.log('Cliked', clickedId, item)
+    if(clickedId === item.id){
+      return true
+    }
+    return false
+  }
+
+  render(){
+    const { clickedId, toolsObject } = this.props
     console.log(clickedId, 'i amd clicked in child')
     console.log('I am the props in child', this.props)
     
     return (
       <div className="tools-slider">
         <button type="button" onClick={this.closeSlider}>Close me</button>
-        <h1>I am Tools Slider</h1>
+        {toolsObject[0].mainTitle}
+        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+          <div className="carousel-inner">
+            { toolsObject.map((item) =>
+              <div className={`carousel-item ${this.getActive(item) ? 'active': ''}`} key={item.id}>
+                <div className="carousel-caption d-none d-md-block">
+                  <p>{item.mainTitle}</p>
+                </div>
+              </div>
+            )}
+          </div>
+          <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
       </div>
   )}
 }
