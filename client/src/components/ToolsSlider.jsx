1bc11135ba0d8027b76bb7e941d46b299b8258d2
abimael-hernandez <abimael.hernandez@adevinta.com>
2020-05-17 12:04:20 -0500

    carousel rndering problems after four slides need to check its own position to render correctly

--- client/src/components/ToolsSlider.jsx
+++ client/src/components/ToolsSlider.jsx
@@ -5,10 +5,27 @@ import Carousel from '@brainhubeu/react-carousel'
 import '@brainhubeu/react-carousel/lib/style.css'
 
 export default class ToolsSlider extends Component {
+  constructor(){
+    super()
+    this.state = {
+      centerItem: {},
+      leftItem: {},
+      rightItem: {},
+      fourthItem: {},
+      fithItem: {},
+      sixthItem: {},
+      seventhItem: {},
+      eightItem: {},
+      ninthItem: {},
+      thenthItem: {}
+    }
+  }
 
-
-  componentDidMount() {   
-    this.getActive() 
+  componentDidMount() {
+    this.getCenterItem()
+    this.getLeftItem()
+    this.getRightItem()
+    this.getFourthItem()
   }
 
   closeSlider = () => {
@@ -16,28 +33,88 @@ export default class ToolsSlider extends Component {
     resetProps(true)
   }
 
-  getActive = () => {
-    const { clickedId } = this.props
-    console.log('i am the cliked id', clickedId)
+  getLeftItem = () => {
+    const { clickedId, toolsObject } = this.props
+    // const length = Object.values(toolsObject).length  === clickedId
+    const test2 = toolsObject.find(x => x.id === clickedId - 1)
+    this.setState({ leftItem: test2})
+  }
+
+  getCenterItem = () => {
+    const { clickedId, toolsObject } = this.props
+    const test = toolsObject.find(x => x.id === clickedId)
+    this.setState({ centerItem: test})
+  }
+
+  getRightItem = () => {
+    const { clickedId, toolsObject } = this.props
+    const length = Object.values(toolsObject).length === clickedId
+    const test3 = length ? toolsObject.find(x => x.id === 1) : toolsObject.find(x => x.id === clickedId + 1)
+    this.setState({ rightItem: test3})
+  }
+
+  getFourthItem = () => {
+    const { clickedId, toolsObject } = this.props
+    const test4 = toolsObject.find(x => x.id === clickedId + 2)
+    this.setState({ fourthItem: test4})
+  }
+
+  getFithItem = () => {
+    const { clickedId, toolsObject } = this.props
+    const test5 = toolsObject.find(x => x.id === clickedId + 3)
+    this.setState({ fithItem: test5})
+  }
+
+  getsixthItem = () => {
+    const { clickedId, toolsObject } = this.props
+    const test6 = toolsObject.find(x => x.id === clickedId + 4)
+    this.setState({ sixthItem: test6})
+  }
+
+  getSeventhItem = () => {
+    const { clickedId, toolsObject } = this.props
+    const test7 = toolsObject.find(x => x.id === clickedId  + 5)
+    this.setState({ seventhItem: test7})
+  }
+
+  getEightItem = () => {
+    const { clickedId, toolsObject } = this.props
+    const test8 = toolsObject.find(x => x.id === clickedId + 6)
+    this.setState({ eightItem: test8})
+  }
+
+  getNithItem = () => {
+    const { clickedId, toolsObject } = this.props
+    const test9 = toolsObject.find(x => x.id === clickedId + 8)
+    this.setState({ fithItem: test9})
+  }
+
+  getTenthItem = () => {
+    const { clickedId, toolsObject } = this.props
+    const test5 = toolsObject.find(x => x.id === clickedId + 9)
+    this.setState({ fithItem: test5})
   }
 
   render(){
-    const { toolsObject } = this.props
-
+    const { leftItem, centerItem, rightItem, fourthItem, fithItem, sixthItem, seventhItem, eightItem, ninthItem,thenthItem } = this.state
+    console.log('hello', leftItem, centerItem, rightItem, fourthItem, fithItem, sixthItem, seventhItem, eightItem, ninthItem,thenthItem)
     const finalArray = [
       (<div key={1}>
-        <h1>{toolsObject[0].mainTitle}</h1>
+        <h1>{leftItem.mainTitle}</h1>
       </div>),
       (<div key={2}>
-        <h1>{toolsObject[1].mainTitle}</h1>
+        <h1>{centerItem.mainTitle}</h1>
       </div>),
       (<div key={3}>
-        <h1>{toolsObject[2].mainTitle}</h1>
+        <h1>{rightItem.mainTitle}</h1>
+      </div>),
+      (<div key={4}>
+        <h1>{fourthItem.mainTitle}</h1>
       </div>)
     ]
 
     console.log('I am the toolsObject', finalArray)
-    
+
     return (
       <div className="tools-slider-parent">
         <div id="tools-slider" className="carousel slide" data-interval="false" data-ride="carousel">
