15f976c552ed048d96d61fd4479b7aa54c8a3459
Javier Elizalde <javier.elizalde@globant.com>
2020-05-17 19:42:53 -0500

    Created and styled completely the dropdown component for the tools section

--- client/src/components/ToolsCards.jsx
+++ client/src/components/ToolsCards.jsx
@@ -29,20 +29,21 @@ resetProps = (value) => {
       return <ToolsSlider toolsObject={toolsObject} clickedId={clickedId} resetProps={this.resetProps}/>
     }
     return (
-      <div className="toolsParent" id="herramientas">
-        <div className="toolsParent-header">
-          <div className="toolsParent-header-title">
+      <div className="tools-parent" id="herramientas">
+        <div className="tools-parent-header">
+          <div className="tools-parent-header-title">
             <h1>Herramientas</h1>
           </div>
-          <div className="toolsParent-header-subText">
+          <div className="tools-parent-header-subText">
             <p>Buscando consolidar los procesos de gestión y consultoría integral, Raiffeisen Latina propone un grupo de herramientas sistematizadas conceptualizadas y desarrolladas por DGRV para el sector cooperativo y de finanzas incluyentes.</p>
           </div>
         </div>
-        <div className="toolsParent-cards card-body">
+        <div className="tools-parent-desktop">
+          <div className="tools-parent-desktop-cards card-body">
           {toolsObject.map((item) => 
-            <div key={item.id} className="toolsParent-cards-card">
-              <div className="toolsParent-cards-card-content" style={{ background: item.backgroundColor }}>
-                <div className="toolsParent-cards-card-content-openButton">
+            <div key={item.id} className="tools-parent-desktop-cards-card">
+              <div className="tools-parent-desktop-cards-card-content" style={{ background: item.backgroundColor }}>
+                <div className="tools-parent-desktop-cards-card-content-openButton">
                   <span 
                     role="button" 
                     tabIndex={0} 
@@ -52,10 +53,30 @@ resetProps = (value) => {
                     &gt;
                   </span>
                 </div>
-                <p className="toolsParent-cards-card-content-title">{item.mainTitle}</p>
+                <p className="tools-parent-desktop-cards-card-content-title">{item.mainTitle}</p>
                 </div>
             </div>
           )}
+        </div>
+        </div>
+        <div className="tools-parent-mobile">
+          {
+            toolsObject.map((item) =>
+              <div key={item.id} className="tools-parent-mobile-dropdown">
+                <button className="btn tools-parent-mobile-dropdown-button" style={{ background: item.backgroundColor }} type="button" data-toggle="collapse" data-target={`#${item.mobileId}`} aria-expanded="false">
+                  <i>{item.mainTitle}</i>
+                  <svg className="bi bi-chevron-down tools-parent-mobile-dropdown-button-drop" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 01.708 0L8 10.293l5.646-5.647a.5.5 0 01.708.708l-6 6a.5.5 0 01-.708 0l-6-6a.5.5 0 010-.708z" clipRule="evenodd"/>
+                  </svg>
+                </button>
+                <div className="collapse tools-parent-mobile-dropdown-content" id={`${item.mobileId}`}>
+                  <div className="card card-body" style={{ background: item.backgroundColor }}>
+                    <p>{item.bodyContent}</p>
+                  </div>
+                </div>
+              </div>
+            )
+          }
         </div>
       </div>
   )}
