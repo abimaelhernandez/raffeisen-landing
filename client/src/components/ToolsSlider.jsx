4b9bf721976f09334462e78eff5d2b45d4cb1ed9
Javier Elizalde <javelisol@gmail.com>
2020-05-23 11:17:57 -0500

    attempt with diff things

--- client/src/components/ToolsSlider.jsx
+++ client/src/components/ToolsSlider.jsx
@@ -5,10 +5,16 @@ import Carousel from '@brainhubeu/react-carousel'
 import '@brainhubeu/react-carousel/lib/style.css'
 
 export default class ToolsSlider extends Component {
+  constructor() {
+    super()
+    this.state = { 
+      value: -1
+    }
+    this.onChange = this.onChange.bind(this)
+  }
 
-
-  componentDidMount() {   
-    this.getActive() 
+  onChange(value) {
+    this.setState({ value })
   }
 
   closeSlider = () => {
@@ -16,33 +22,53 @@ export default class ToolsSlider extends Component {
     resetProps(true)
   }
 
-  getActive = () => {
-    const { clickedId } = this.props
-    console.log('i am the cliked id', clickedId)
-  }
-
   render(){
-    const { toolsObject } = this.props
-
-    const finalArray = [
-      (<div key={1}>
-        <h1>{toolsObject[0].mainTitle}</h1>
-      </div>),
-      (<div key={2}>
-        <h1>{toolsObject[1].mainTitle}</h1>
-      </div>),
-      (<div key={3}>
-        <h1>{toolsObject[2].mainTitle}</h1>
-      </div>)
-    ]
-
-    console.log('I am the toolsObject', finalArray)
+    const { toolsObject, clickedId } = this.props
+    const { value } = this.state
+    console.log('I am in the slider and clicked id', clickedId)
     
     return (
       <div className="tools-slider-parent">
         <div id="tools-slider" className="carousel slide" data-interval="false" data-ride="carousel">
           <span role="button" tabIndex={0} onClick={this.closeSlider} onKeyDown={this.closeSlider}>Close me</span>
-          <Carousel arrows infinite slidesPerPage={3} slides={finalArray}/>
+          <Carousel
+            value={value}
+            onChange={this.onChange}
+            slides={[
+              (<div key={1}>
+                <h1>{toolsObject[0].mainTitle}</h1>
+              </div>),
+              (<div key={2}>
+                <h1>{toolsObject[1].mainTitle}</h1>
+              </div>),
+              (<div key={3}>
+                <h1>{toolsObject[2].mainTitle}</h1>
+              </div>),
+              (<div key={4}>
+                <h1>{toolsObject[3].mainTitle}</h1>
+              </div>),
+              (<div key={5}>
+                <h1>{toolsObject[4].mainTitle}</h1>
+              </div>),
+              (<div key={6}>
+                <h1>{toolsObject[5].mainTitle}</h1>
+              </div>),
+              (<div key={7}>
+                <h1>{toolsObject[6].mainTitle}</h1>
+              </div>),
+              (<div key={8}>
+                <h1>{toolsObject[7].mainTitle}</h1>
+              </div>),
+              (<div key={9}>
+                <h1>{toolsObject[8].mainTitle}</h1>
+              </div>),
+              (<div key={10}>
+                <h1>{toolsObject[9].mainTitle}</h1>
+              </div>)
+            ]}
+            arrows
+            infinite
+          />
         </div>
       </div>
   )}
