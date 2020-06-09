f28df3403a72263969c500de3445600e0bd777f5
abimael-hernandez <abimael.hernandez@adevinta.com>
2020-06-08 23:22:17 -0500

    remove padding

--- client/src/components/Services.jsx
+++ client/src/components/Services.jsx
@@ -54,7 +54,8 @@ export default class Services extends Component {
                  <div className="clickHandler">
                     <button
                       type="button"
-                      className="service-info-more" onClick={this.clickHandler.bind(this, item.id)}>
+                      className="service-info-more" onClick={this.clickHandler.bind(this, item.id)}
+                    >
                       {item.button}
                       <svg className="service-info-more-icon" viewBox="0 0 16 16" fill="currentColor">
                         <path d="M4.646 1.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L10.293 8 4.646 2.354a.5.5 0 010-.708z"/>
