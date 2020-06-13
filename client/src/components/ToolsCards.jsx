e95d9aca88f27c1391a97d3e4036c3bbdedbd7ae
Javier Elizalde <javelisol@gmail.com>
2020-06-12 23:50:38 -0500

    removed unsed onclick on tools cards

--- client/src/components/ToolsCards.jsx
+++ client/src/components/ToolsCards.jsx
@@ -58,7 +58,6 @@ resetProps = (value) => {
                   <button
                     type="button"
                     className="btn"
-                    onClick={this.clickHandler.bind(this, item.id)}
                   >
                     <svg className="bi bi-chevron-right nextArrrow" viewBox="0 0 16 16" fill="currentColor">
                       <path d="M4.646 1.646a.5.5 0 01.708 0l6 6a.5.5 0 010 .708l-6 6a.5.5 0 01-.708-.708L10.293 8 4.646 2.354a.5.5 0 010-.708z"/>
