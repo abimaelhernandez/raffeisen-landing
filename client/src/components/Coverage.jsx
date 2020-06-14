50fdb69e80bc26179786acb163db07be4fcd9ad7
miriam30 <miriam.alvarez.p96@gmail.com>
2020-06-14 16:39:03 -0500

    use style variables and omptimize svg

--- client/src/components/Coverage.jsx
+++ client/src/components/Coverage.jsx
@@ -1,39 +1,42 @@
-import React,{ Component } from 'react'
-// import Fade from 'react-reveal/Fade'
-// import Roll from 'react-reveal/Roll'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
 export default class Coverage extends Component {
   render() {
-    const { coverageInfo } = this.props
+    const {coverageInfo} = this.props
     return (<div className="rl_coverage" id="cobertura">
 
       <div className="rl_coverage_description">
-        <h2 className="rl_coverage-title" >{ coverageInfo.title }</h2>
-        <p className="rl_coverage-text">{ coverageInfo.description }</p>
+        <div className="rl_coverage_description-icon" style={{
+            backgroundImage: 'url("../assets/icons/square-icon.svg")'
+          }}/>
+        <h2 className="rl_coverage-title">{coverageInfo.title}</h2>
+        <p className="rl_coverage-text">{coverageInfo.description}</p>
       </div>
-      <div
-        className="rl_coverage-map-img"
-        style={{backgroundImage: 'url("../assets/images/coverage_map.svg")'}}
-      >
-      <div className="rl_coverage-map-countries">
-        {
-          coverageInfo.countries.map((country) =>
-          country.sections ? <div className="rl_coverage-map-countries-section">
-          {country.sections.map((place) =>
-            <span key={`${place.name}-${place.style}`} className={`dot ${place.style}`}><span className="country-name">{ place.name }</span></span>
-          )}
-          </div>:
-          <span key={`${country.name}-${country.style}`} className={`dot ${country.style}`}><span className="country-name">{ country.name }</span></span>)
-        }
+      <div className="rl_coverage-map-img" style={{
+          backgroundImage: 'url("../assets/images/coverage_map.svg")'
+        }}>
+        <div className="rl_coverage-map-countries">
+          {
+            coverageInfo.countries.map(
+              (country) => country.sections
+              ? <div className="rl_coverage-map-countries-section">
+                {
+                  country.sections.map((place) => <span key={`${place.name}-${place.style}`} className={`dot ${place.style}`}>
+                    <span className="country-name">{place.name}</span>
+                  </span>)
+                }
+              </div>
+              : <span key={`${country.name}-${country.style}`} className={`dot ${country.style}`}>
+                <span className="country-name">{country.name}</span>
+              </span>)
+          }
+        </div>
       </div>
-
-    </div>
-
     </div>)
   }
 }
 
 Coverage.propTypes = {
-  coverageInfo: PropTypes.instanceOf(Object),
+  coverageInfo: PropTypes.instanceOf(Object)
 }
